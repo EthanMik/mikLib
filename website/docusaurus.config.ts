eba57ab95c34b0c2f2ba4bc8b35923e954d1e943
EthanMik <mikolaycikethan@gmail.com>
2026-06-09 18:23:18 -0500

    Updated Cards

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'mikLib Docs',
+  tagline: 'An easy to use odometry VEXcode template with UI support.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
